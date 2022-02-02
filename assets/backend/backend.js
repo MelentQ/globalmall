5e3b998f08a1e15b3a983cee40bf066deee39643
MelentQ <mejleht@yandex.ru>
2022-02-02 22:51:04 +0300

    Updates

--- assets/backend/backend.js
+++ assets/backend/backend.js
@@ -1,3 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+  searchFormSubmitHandler()
 });
+
+/**
+ * Сабмит формы поиска на страницах списка элементов
+ */
+function searchFormSubmitHandler() {
+  const form = document.querySelector('.js-backend-submit-handler');
+  if (form) {
+    const resetBtn = form.querySelector('.js-reset');
+    resetBtn.addEventListener('click', () => {
+      form.reset();
+      submitForm();
+    })
+
+    function submitForm() {
+      form.submit();
+    }
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      console.log('submit');
+    })
+  }
+}
