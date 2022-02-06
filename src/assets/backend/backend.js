209b6c4cf23998f0f432d5b20022d4c795f47f9f
MelentQ <mejleht@yandex.ru>
2022-02-06 15:56:43 +0300

    Деталка новости, "Показать ещё", слайдер в детальной

--- src/assets/backend/backend.js
+++ src/assets/backend/backend.js
@@ -1,28 +1,111 @@
 document.addEventListener('DOMContentLoaded', () => {
   searchFormSubmitHandler()
+  showMore()
 });
 
 /**
  * Сабмит формы поиска на страницах списка элементов
+ * Универсальная функция
  */
 function searchFormSubmitHandler() {
-  const form = document.querySelector('.js-backend-submit-handler');
-  if (form) {
-    const resetBtn = form.querySelector('.js-reset');
-    resetBtn.addEventListener('click', () => {
-      form.reset();
-      submitForm();
-    })
+  const forms = Array.from(document.querySelectorAll('.js-backend-submit-handler'));
 
-    function submitForm() {
-      // ОНО НЕ РАБОТАЕТ
-      // НАДО У АРТЁМА УЗНАТЬ ЧТО ДЕЛАТЬ В ЭТОЙ СИТУАЦИИ
-      form.submit();
-    }
-
+  forms.forEach(form => {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
-      console.log('submit');
+      get(form);
     })
+
+    const resetBtn = form.querySelector('.js-reset');
+    if(resetBtn) {
+      resetBtn.addEventListener('click', () => {
+        form.reset();
+        get(form);
+      })
+    }
+
+    const radioButtons = Array.from(form.querySelectorAll('.js-radio'))
+    const checkboxes = Array.from(form.querySelectorAll('.js-checkbox'))
+    const formInputs = [...radioButtons, ...checkboxes];
+    formInputs.forEach(input => {
+      input.addEventListener('change', () => {
+        get(form);
+      })
+    })
+  })
+
+  // Основная логика
+  function get(form) {
+    // const url = form.action;
+    console.log('backend.js')
   }
+}
+
+/**
+ * Кнопка "Показать ещё"
+ * Универсальная функция
+ * URL для запроса вставлять в href кнопки
+ */
+function showMore() {
+  const ajaxContainers = Array.from(document.querySelectorAll('.js-ajax-show-more-container'));
+  ajaxContainers.forEach((container, i) => {
+    const elementsContainer = container.querySelector('.js-ajax-elements-container');
+    const showMoreBtnContainer = container.querySelector('.js-ajax-show-more-btn-container');
+    const showMoreBtn = showMoreBtnContainer.querySelector('.js-ajax-show-more-btn');
+
+    if (!elementsContainer && !showMoreBtnContainer && !showMoreBtn) return;
+
+    showMoreBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+
+      // Блокируем кнопку
+      showMoreBtn.setAttribute('disabled', 'true');
+      showMoreBtn.classList.add('disabled');
+
+      // Добавляем класс контейнеру во время загрузки данных
+      elementsContainer.classList.add('loading');
+
+      const url = showMoreBtn.href || window.location.href;
+
+      fetch(url, {
+        method: 'GET',
+        mode: 'no-cors',
+        headers: {
+          'Content-Type': 'text/html'
+        }
+      })
+        .then(res => res.text())
+        .then(data => {
+          const parser = new DOMParser();
+          const page = parser.parseFromString(data, "text/html");
+
+          // Ищем все ajax контейнеры,
+          // из них понадобится только один с текущим индексом i
+          const newAjaxContainers = page.querySelectorAll('.js-ajax-show-more-container');
+
+          // Находим элементы, вставляем в текущую разметку
+          const newElementsContainer = newAjaxContainers[i].querySelector('.js-ajax-elements-container');
+          const newElements = newElementsContainer.querySelectorAll('.js-ajax-element');
+
+          newElements.forEach(element => {
+            elementsContainer.append(element);
+          });
+
+          // Находим новую кнопку "Показать ещё", обновляем ее href
+          const newShowMoreBtn = newAjaxContainers[i].querySelector('.js-ajax-show-more-btn');
+          showMoreBtn.href = newShowMoreBtn.href;
+        })
+        .catch(function (err) {
+          console.warn('Ошибка при загрузке новых карточек.', err);
+        })
+        .finally(() => {
+          // Разблокируем кнопку
+          showMoreBtn.removeAttribute("disabled");
+          showMoreBtn.classList.remove('disabled');
+
+          // Удаляем класс для загрузки
+          elementsContainer.classList.remove('loading');
+        })
+    })
+  })
 }
--- src/js/custom/initModals.js
+++ src/js/custom/initModals.js
@@ -2,6 +2,18 @@ import '../libs/hystmodal.min.js';
 
 export default function initModals() {
   window.modalAPI = new HystModal({
-    linkAttributeName: "data-hystmodal"
+    linkAttributeName: "data-hystmodal",
+    beforeOpen: (modal) => {
+      imageModal(modal);
+    }
   });
+}
+
+function imageModal(modal) {
+  if (modal.openedWindow.id == "image-modal") {
+    const image = modal.starter.querySelector('.js-image');
+    const modalImageElement = modal.openedWindow.querySelector('.js-image');
+    modalImageElement.src = image.src;
+    modalImageElement.alt = image.alt;
+  }
 }
