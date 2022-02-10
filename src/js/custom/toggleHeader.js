a207ad310e2cfd4aab3e030f984fe9fdd3173d76
MelentQ <mejleht@yandex.ru>
2022-02-11 01:45:30 +0300

    404

--- src/js/custom/toggleHeader.js
+++ src/js/custom/toggleHeader.js
@@ -1,11 +1,11 @@
 export default function toggleHeader() {
   const header = document.querySelector('.header');
   if(header) {
+    const mainBlock = header.querySelector('.header__main-block');
     if (document.documentElement.clientWidth > 1024) {
       const secondaryBlock = header.querySelector('.header__secondary-block');
       document.documentElement.style.setProperty('--js-secondary-header-height', secondaryBlock.clientHeight / 10 + 'rem');
 
-      const mainBlock = header.querySelector('.header__main-block');
       document.documentElement.style.setProperty('--js-main-header-height', mainBlock.clientHeight / 10 + 'rem');
 
       const toggleY = header.dataset.toggle || 300;
@@ -22,6 +22,7 @@ export default function toggleHeader() {
       }, false)
     } else {
       document.documentElement.style.setProperty('--js-secondary-header-height', '0');
+      document.documentElement.style.setProperty('--js-main-header-height', mainBlock.clientHeight / 10 + 'rem');
     }
     
   }
