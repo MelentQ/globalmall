7235af73123cb16cf8f60602beaa6ede1cfeb903
MelentQ <mejleht@yandex.ru>
2022-02-17 00:21:45 +0300

    Правки от 16.02

--- src/js/custom/initModals.js
+++ src/js/custom/initModals.js
@@ -4,7 +4,7 @@ export default function initModals() {
   window.modalApi = new HystModal({
     linkAttributeName: "data-hystmodal",
     beforeOpen: (modal) => {
-      // imageModal(modal);
+      imageModal(modal);
       playVideo(modal);
     },
     afterClose: (modal) => {
@@ -15,8 +15,9 @@ export default function initModals() {
 
 function imageModal(modal) {
   if (modal.openedWindow.id == "image-modal") {
-    const image = modal.starter.querySelector('.js-image');
+    const image = modal.starter.querySelector('.js-image-modal');
     const modalImageElement = modal.openedWindow.querySelector('.js-image');
+
     if (image && modalImageElement) {
       modalImageElement.src = image.src;
       modalImageElement.alt = image.alt;
